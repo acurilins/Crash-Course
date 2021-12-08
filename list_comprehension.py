fca77702815ebfc5e0e13cb66bf133ee8d5fa07a
acurilins <68602525+acurilins@users.noreply.github.com>
2021-12-08 10:47:40 +0400

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -8,3 +8,10 @@
 
 multiples = [ x*7 for x in range (1, 11)]
 print(multiples)
+
+languages = ["Python", "Perl", "Ruby", "Java", "Go", "C"]
+lengths = [len(language) for language in languages]
+print(lengths)
+
+z = [x for x in range(0,101) if x % 3 == 0]
+print(z)
